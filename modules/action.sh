330f84b74f49d0da1cd71cb181685d775ecce61f
Liliya <76985102+Liliya2727@users.noreply.github.com>
2025-03-04 07:25:17 +0700

    Update action.sh

--- modules/action.sh
+++ modules/action.sh
@@ -1,1 +1,35 @@
+#!/system/bin/sh
+MODDIR=${0%/*}
+MODULE_PROP="$MODDIR/module.prop"
+bypassflag="/data/local/tmp/tempbypass"
 
+# Function to print messages with delay
+zepassprint() {
+    echo ""
+    echo "        ZEPASSCHARGE          "
+    echo ""
+    sleep 1
+}
+
+zepassprint
+
+if [ -f "$bypassflag" ]; then
+    echo "Enabling bypass charge..."
+    sleep 4
+    rm -f "$bypassflag"
+    echo "Bypass is now Enabled!"
+    sleep 3
+    sed -i "s|^description=.*|description=Status • Enabled✅|" "$MODULE_PROP"
+else
+    echo "Disabling bypass charge..."
+    sleep 4
+    touch "$bypassflag"
+    echo "Bypass is now Disabled!"
+    sleep 3
+    sed -i "s|^description=.*|description=Status • Disabled❌|" "$MODULE_PROP"
+fi
+
+sleep 1
+echo ""
+echo "Done..."
+sleep 2

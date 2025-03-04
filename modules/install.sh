81be64f7df4a0c88c230a0077c28a92eed6ce3e5
Liliya <76985102+Liliya2727@users.noreply.github.com>
2025-03-04 12:29:58 +0700

    T2 Install.sh

--- modules/install.sh
+++ modules/install.sh
@@ -66,16 +66,20 @@ ui_print "- Start installing..."
 sleep 2
 # Continue with installation
 ui_print
-ui_print "       ZEPASSCHARGE Type3!        "
+ui_print "       ZEPASSCHARGE Type2!        "
 ui_print 
 ui_print "- by : @Zexshia!"
 sleep 1
 ui_print "- Device : $(getprop ro.product.board) "
 sleep 2
 ui_print "- Extracting module files"
-touch "/data/local/tmp/tempbypass"
+mkdir /data/ZepassCharge
 unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
 unzip -o "$ZIPFILE" 'service.sh' -d "$MODPATH" >&2
-unzip -o "$ZIPFILE" 'action.sh' -d $MODPATH >&2
+unzip -o "$ZIPFILE" 'gamelist.txt' -d "/data/ZepassCharge" >&2
 
+ui_print "- Installing bellavita toast"
+unzip -o "$ZIPFILE" 'toast.apk' -d $MODPATH >&2
+pm install $MODPATH/toast.apk
+rm $MODPATH/toast.apk
 set_perm_recursive $MODPATH 0 0 0777 0777
